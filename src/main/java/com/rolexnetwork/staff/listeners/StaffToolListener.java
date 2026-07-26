60e0f44aa69c61795cbdf214ce02e725810422cc
harelamram3-ctrl <harelamram3@gmail.com>
2026-07-26 22:26:16 +0300

    Create StaffToolListener.java

---
+++ src/main/java/com/rolexnetwork/staff/listeners/StaffToolListener.java
@@ -0,0 +1,56 @@
+package com.rolexnetwork.staff.listeners;
+
+import com.rolexnetwork.staff.RolexStaff;
+import com.rolexnetwork.staff.managers.StaffManager;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class StaffToolListener implements Listener {
+
+    private final StaffManager staffManager;
+
+    public StaffToolListener(StaffManager staffManager) {
+        this.staffManager = staffManager;
+    }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        Player staff = event.getPlayer();
+
+        // בדיקה אם איש הצוות נמצא ב-Staff Mode
+        if (!staffManager.isInStaffMode(staff)) {
+            return;
+        }
+
+        // בדיקה שהאינטראקציה היא מול שחקן אחר
+        if (!(event.getRightClicked() instanceof Player)) {
+            return;
+        }
+
+        Player target = (Player) event.getRightClicked();
+        ItemStack handItem = staff.getInventory().getItemInMainHand();
+
+        if (handItem == null || handItem.getType() == Material.AIR) {
+            return;
+        }
+
+        // 1. אולר שוויצרי (Nether Star) - פתיחת תפריט מהיר / ניהול
+        if (handItem.getType() == Material.NETHER_STAR) {
+            event.setCancelled(true);
+            staff.sendMessage(ChatColor.GOLD + "[RolexStaff] נפתח תפריט ניהול עבור השחקן: " + ChatColor.WHITE + target.getName());
+            // כאן נפתח בהמשך את ה-GUI של העונשים!
+        }
+
+        // 2. כלי הקפאה (Ice) - הקפאת שחקן במקום
+        if (handItem.getType() == Material.ICE) {
+            event.setCancelled(true);
+            staff.sendMessage(ChatColor.AQUA + "[RolexStaff] הקפאת את השחקן: " + ChatColor.WHITE + target.getName());
+            target.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "הוקפאת על ידי איש צוות! אל תתנתק מהשרת!");
+        }
+    }
+}
