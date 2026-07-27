1699762298ecb85753ed91df96c7f26aea215cee
harelamram3-ctrl <harelamram3@gmail.com>
2026-07-27 10:14:19 +0300

    Update MenuClickListener.java

--- src/main/java/com/rolexnetwork/staff/listeners/MenuClickListener.java
+++ src/main/java/com/rolexnetwork/staff/listeners/MenuClickListener.java
@@ -1,85 +1,46 @@
 package com.rolexnetwork.staff.listeners;
 
 import com.rolexnetwork.staff.RolexStaff;
-import com.rolexnetwork.staff.enums.StaffRank;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class MenuClickListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getView().getTitle().startsWith(ChatColor.DARK_GRAY + "RolexStaff - ")) {
+        if (event.getView().getTitle().startsWith(ChatColor.DARK_RED + "Staff Menu: ")) {
             event.setCancelled(true);
 
             if (event.getCurrentItem() == null) return;
 
             Player staff = (Player) event.getWhoClicked();
-            ItemStack clickedItem = event.getCurrentItem();
-
-            String targetName = event.getView().getTitle().replace(ChatColor.DARK_GRAY + "RolexStaff - ", "");
+            String targetName = event.getView().getTitle().replace(ChatColor.DARK_RED + "Staff Menu: ", "");
             Player target = Bukkit.getPlayer(targetName);
 
             if (target == null) {
-                staff.sendMessage(ChatColor.RED + "[RolexStaff] השחקן כבר אינו מחובר לשרת.");
+                staff.sendMessage("§cהשחקן אינו מחובר כעת.");
                 staff.closeInventory();
                 return;
             }
 
-            StaffRank staffRank = StaffRank.getRank(staff);
-
-            switch (clickedItem.getType()) {
-                case PACKED_ICE: // הקפאה / ביטול הקפאה
-                    boolean isNowFrozen = RolexStaff.getInstance().getFreezeManager().toggleFreeze(target);
-                    
-                    if (isNowFrozen) {
-                        staff.sendMessage(ChatColor.AQUA + "[RolexStaff] הקפאת את " + target.getName());
-                        target.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "הוקפאת על ידי איש צוות! אל תתנתק!");
-                    } else {
-                        staff.sendMessage(ChatColor.GREEN + "[RolexStaff] ביטלת את ההקפאה של " + target.getName());
-                        target.sendMessage(ChatColor.GREEN + "ההקפאה שלך בוטלה.");
-                    }
-                    
-                    RolexStaff.getInstance().getDiscordManager().sendPunishmentLog(
-                        staff.getName(), target.getName(), isNowFrozen ? "FREEZE" : "UNFREEZE", "שינוי מצב הקפאה דרך ה-GUI"
-                    );
-                    
+            switch (event.getCurrentItem().getType()) {
+                case PACKED_ICE:
+                    RolexStaff.getInstance().getFreezeManager().toggleFreeze(target);
+                    staff.sendMessage("§e[RolexStaff] שינית את מצב ההקפאה של " + target.getName());
                     staff.closeInventory();
                     break;
 
-                case LEATHER_BOOTS: // Kick
-                    target.kickPlayer(ChatColor.RED + "נזרקת מהשרת על ידי איש צוות (RolexStaff)");
-                    staff.sendMessage(ChatColor.GREEN + "[RolexStaff] נזרק מהשרת: " + target.getName());
-                    
-                    RolexStaff.getInstance().getDiscordManager().sendPunishmentLog(
-                        staff.getName(), target.getName(), "KICK", "הנפה מהשרת דרך RolexStaff GUI"
-                    );
-                    
-                    staff.closeInventory();
+                case CHEST:
+                    staff.openInventory(target.getInventory());
                     break;
 
-                case REDSTONE_BLOCK: // Ban
-                    if (staffRank.getLevel() >= 2) {
-                        Bukkit.getBanList(org.bukkit.BanList.Type.NAME).addBan(
-                                target.getName(),
-                                ChatColor.RED + "נחסמת מהשרת על ידי צוות RolexNetWork!",
-                                null,
-                                staff.getName()
-                        );
-                        target.kickPlayer(ChatColor.RED + "נחסמת מהשרת על ידי איש צוות!");
-                        staff.sendMessage(ChatColor.DARK_RED + "[RolexStaff] השחקן " + target.getName() + " נחסם בהצלחה!");
-                        
-                        RolexStaff.getInstance().getDiscordManager().sendPunishmentLog(
-                            staff.getName(), target.getName(), "BAN", "חסימה מהשרת דרך RolexStaff GUI"
-                        );
-                    } else {
-                        staff.sendMessage(ChatColor.RED + "[RolexStaff] אין לך הרשאה לחסום שחקנים!");
-                    }
+                case ENDER_PEARL:
+                    staff.teleport(target.getLocation());
+                    staff.sendMessage("§a[RolexStaff] השתגרת אל " + target.getName());
                     staff.closeInventory();
                     break;
 
