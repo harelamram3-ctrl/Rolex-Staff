8a80afdecf606a7740330b2b0e4b7b9e2684a2cb
harelamram3-ctrl <harelamram3@gmail.com>
2026-07-27 10:13:21 +0300

    Update FreezeManager.java

--- src/main/java/com/rolexnetwork/staff/managers/FreezeManager.java
+++ src/main/java/com/rolexnetwork/staff/managers/FreezeManager.java
@@ -1,29 +1,25 @@
 package com.rolexnetwork.staff.managers;
 
 import org.bukkit.entity.Player;
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 public class FreezeManager {
 
-    // רשימה של כל ה-UUIDs של השחקנים שמוקפאים כרגע
     private final Set<UUID> frozenPlayers = new HashSet<>();
 
-    // האם השחקן מוקפא?
     public boolean isFrozen(Player player) {
         return frozenPlayers.contains(player.getUniqueId());
     }
 
-    // הקפאה או ביטול הקפאה (Toggle)
-    public boolean toggleFreeze(Player player) {
-        if (isFrozen(player)) {
-            frozenPlayers.remove(player.getUniqueId());
-            return false; // מוקפא לבאטל (חופשי)
+    public void toggleFreeze(Player target) {
+        if (isFrozen(target)) {
+            frozenPlayers.remove(target.getUniqueId());
+            target.sendMessage("§a[RolexStaff] הופשרה ההקפאה שלך!");
         } else {
-            frozenPlayers.add(player.getUniqueId());
-            return true; // כעת מוקפא
+            frozenPlayers.add(target.getUniqueId());
+            target.sendMessage("§c[RolexStaff] הוקפאת על ידי איש צוות! אל תתנתק!");
         }
     }
 }
